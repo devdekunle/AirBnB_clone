10d4b6851bce455fc37f6ea85857e0517fec71b8
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-02-08 00:12:52 +0100

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,19 @@ class BaseModel:
     other classes to be used in the application
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Method for object instantiation of the BaseModel"""
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
+
 
     def __str__(self):
         """Method to make the instance printable"""
@@ -30,8 +38,10 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary containing all key/values of the instance"""
-        obj_dict = self.__dict__.copy()
+        obj_dict = self.__dict__.copy() #assign copy of object atrributes
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         obj_dict['__class__'] = self.__class__.__name__
         return obj_dict
+
+
